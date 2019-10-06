43f9d2d15009682e7f654d5cb0a4737a2f872af0
jay vyas <jayunit100@jvyas-a01.vmware.com>
2019-10-06 10:53:28 -0400

    K8s recipe, initial commit not done yet

---
+++ k8s/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "This recipe setsup a k8s cluster w/ calico"
+
+function setup() {
+    update-alternatives --set iptables /usr/sbin/iptables-legacy
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum install -y docker-ce docker-ce-cli containerd.io
+}
+
+function k8s() {
+    swapoff -a
+    # Set SELinux in permissive mode (effectively disabling it)
+    setenforce 0
+    source /vagrant/kubeadmrepo.sh
+    sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+    yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+    systemctl enable --now kubelet
+}
+
+function network() {
+    echo "blah"
+}
+
+setup
+k8s
+
